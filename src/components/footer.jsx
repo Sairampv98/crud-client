968a60e0c0911da7bd4e584fe7754df03c2406bf
Sairampv98 <ramsaipv3@gmail.com>
2025-12-18 02:26:48 -0800

    added navbar, footer, flexbox

---
+++ src/components/footer.jsx
@@ -0,0 +1,34 @@
+import "./footer.css"
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>©Sai 2025-2026</p>
+      <ul>
+        <li>
+          <a href="#">Terms and Conditions</a>
+        </li>
+        <li>
+          <a href="#">Bunny&Buddy</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">People</a>
+        </li>
+        <li>
+          <a href="#">Others</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">People</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#">Others</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
